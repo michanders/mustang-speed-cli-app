4976ed1cc99eb0a342f9c84b85cdd0fad5ac0468
Michael Anderson <michanders@msn.com>
2017-11-22 21:39:48 +0000

    Still working on mustang scraper. Year attribute proves a challenge.

--- lib/mustang_speed/mustang_scraper.rb
+++ lib/mustang_speed/mustang_scraper.rb
@@ -16,8 +16,28 @@ class MustangSpeed::MustangScraper
     @@years
   end
 
+  def self.check_for_mustang(stang)
+    x = true
+    self.all.each do |car|
+      if car.name == stang.name && car.year == stang.year
+        x = false
+      end
+    end
+    x
+  end
+
   def self.mustang_scrape
     doc = Nokogiri::HTML(open("https://www.0-60specs.com/ford-mustang-0-60-times/"))
+    doc.css(".row tr").each do |stang|
+      if stang.css(".column-6").text.length > 15
+        mustang = self.new
+        mustang.name = stang.css(".column-1").text
+        mustang.year = "2017"
+        mustang.to_sixty = stang.css(".column-5").text
+        mustang.quarter_mile = stang.css(".column-6").text
+        self.all << mustang
+      end
+    end
     binding.pry
 
   end
