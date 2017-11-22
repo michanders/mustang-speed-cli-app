f5b980ab8d94e45dd9cf7ed5a9ad602c3c89f7de
Michael Anderson <michanders@msn.com>
2017-11-22 21:14:12 +0000

    Shaping the CLI class. App is running correctly.

--- lib/mustang_speed/cli.rb
+++ lib/mustang_speed/cli.rb
@@ -1,9 +1,25 @@
 class MustangSpeed::CLI
 
   def start
-    puts "Hello"
-
+    opening
+    #run scraper to store objects in all
+    play
+    finish
   end
 
+  def opening
+    puts " "
+    puts "Welcome to the Mustang 0-60 and 1/4 mile times, app."
+  end
+
+  def play
+    puts "Please Enter a year: 1964 - 2017"
+    #shows list of available mustang trims based on user input
+    # enter a number from numbered list of mustang trims from above year
+    # get 0-60 and 1/4 mile times displayed
+    # asks to pick new trim from same year
+    # asks for a new year if above is no
+    # ends if above is no
+  end
 
 end
